3aa4d4dab893864d744c42c9be5c8b16b88d15fa
Trevor <trevortippery221@gmail.com>
2022-07-30 16:00:07 -0700

    Added correct capitalization function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+
+function getComputerChoice() {
+
+}
+
+function playRound (playerSelection, computerSelection) {
+
+}
+
+
+function properCapitalization (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
