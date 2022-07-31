3f140a0bb438945f27d2c7f79e179600b23c751d
Trevor <trevortippery221@gmail.com>
2022-07-30 18:43:19 -0700

    Updated some changes to game

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //  Rock Paper Scissors Game!
 // Trevor Tippery 
 
+/* Functions */
+
 function getComputerChoice() 
 {
     let randomChoice = Math.floor(Math.random() * 3);
@@ -19,27 +21,69 @@ function playRound(playerSelection, computerSelection)
         return `Its a tie! You and the computer both picked ${playerSelection}`
 
     if(playerSelection == "rock" && computerSelection == "scissors")
+    {
         return "You win! Rock beats Scissors";
+    }
     if(playerSelection == "paper" && computerSelection == "rock")
+    {
         return "You win! Paper beats Rock";
+    }
     if(playerSelection == "scissors" && computerSelection == "paper")
+    {
         return "You win! Scissors beats Paper";
+    }
     else
+    {
         return `You lose. The computer chose ${computerSelection} and ${computerSelection} beats ${playerSelection}.`;
+    }
 }
 
-function properCapitalization(string) 
+function properCapitalization() 
 {
-    return string.toLowerCase();
+    let selection = prompt("Choose: Rock, Paper, or Scissors?");
+    if(selection.toLowerCase() == "rock")
+        return selection.toLowerCase();
+    if(selection.toLowerCase() == "paper")
+        return selection.toLowerCase();
+    if(selection.toLowerCase() == "scissors")
+        return selection.toLowerCase();
+    
+    alert("Not a valid choice!");
+    return "error";
 }
 
-let playerSelection;
-let computerSelection;
+function game()
+{
+    let roundCounter = 1;
+    let playerScore = 0;
+    let computerScore = 0;
 
-playerSelection = prompt("Choose: Rock, Paper, or Scissors?");
-playerSelection = properCapitalization(playerSelection);
-computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+    for(let i = 0; i < 5; i++)
+    {
+        let playerSelection;
+        let computerSelection;
+
+        playerSelection = properCapitalization();
+
+        while(playerSelection == "error")
+            playerSelection = properCapitalization();
+
+        computerSelection = getComputerChoice();
+
+        console.log("Round " + i + ": " + playRound(playerSelection, computerSelection));
+
+        console.log(`Score: ${playerScore} - ${computerScore}`);
+        roundCounter++;
+    }
+
+
+}
+
+/* Main body */
+
+console.log("5 rounds of Rock Paper Scissors! Win 3 rounds to win the game.");
+
+game();
 
 
